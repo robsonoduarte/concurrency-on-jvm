e4ecfadfd3310b1916f856bddd2fb4d31bbf05b6
Robson Duarte <robson.o.d@gmail.com>
2015-05-12 22:46:58 -0300

    add class example test.

---
+++ javaee-concurrency-api/src/test/java/br/com/mystudies/javaee/concurrency/api/TesteExample.java
@@ -0,0 +1,19 @@
+package br.com.mystudies.javaee.concurrency.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteExample {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
