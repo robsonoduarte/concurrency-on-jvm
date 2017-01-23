b7abf620c6cf8dde30a282ca10d96316169e6c11
Robson Duarte <robson.o.d@gmail.com>
2017-01-23 20:39:30 -0200

    initial project.

---
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability_thread_safety/App.java
@@ -0,0 +1,13 @@
+package br.com.mystudies.scalability_thread_safety;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
