fab69e69f8ed2e89367e928bdf1c8878d21b8c86
Robson Duarte <robson.o.d@gmail.com>
2017-02-02 21:38:37 -0200

    add main method to test program.

--- scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/TotalFileSizeSequencial.java
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/TotalFileSizeSequencial.java
@@ -1,5 +1,7 @@
 package br.com.mystudies.scalability.thread.safety;
 
+import static java.lang.System.nanoTime;
+import static java.lang.System.out;
 import static java.util.stream.Collectors.toList;
 
 import java.io.File;
@@ -13,11 +15,23 @@ public class TotalFileSizeSequencial {
 
 	public long getTotalSizeOfFilesInDir(File file) throws IOException {
 		return Files.walk(Paths.get(file.toURI()))
+				.peek(out::println)
 				.collect(toList())
 				.size();
 
 	}
 
+
+
+
+	public static void main(String[] args) throws IOException {
+
+		final long start = nanoTime();
+		final long total = new TotalFileSizeSequencial().getTotalSizeOfFilesInDir(new File("E:/"));
+		final long end  = nanoTime();
+		System.out.println("Total Size: " + total);
+		System.out.println("Time taken: " + (end - start) / 1.0e9);
+	}
 
 
 
