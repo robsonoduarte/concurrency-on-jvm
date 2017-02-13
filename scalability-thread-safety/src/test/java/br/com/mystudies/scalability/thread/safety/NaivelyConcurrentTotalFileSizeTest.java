9ea7e29cd9b512cf23eaa997da72e15761735ac7
Robson Duarte <robson.o.d@gmail.com>
2017-02-13 17:35:07 -0200

    start the example using the concurrent.

---
+++ scalability-thread-safety/src/test/java/br/com/mystudies/scalability/thread/safety/NaivelyConcurrentTotalFileSizeTest.java
@@ -0,0 +1,18 @@
+package br.com.mystudies.scalability.thread.safety;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NaivelyConcurrentTotalFileSizeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+	@Test
+	public void test() {
+		/*concurrent.getTotalSizeOfFilesInDir( )*/
+	}
+
+}
