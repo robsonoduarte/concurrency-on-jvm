54ad22707be05ce2badb1ecb9a6a5fcf01e90ced
Robson Duarte <robson.o.d@gmail.com>
2017-06-12 17:40:01 -0300

    add Concurrent example ( start )

--- scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/ConcurrentTotalFileSize.java
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/ConcurrentTotalFileSize.java
@@ -1,16 +1,66 @@
 package br.com.mystudies.scalability.thread.safety;
 
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class ConcurrentTotalFileSize {
 
 
 	public double getTotalSizeOfFile(File path) {
-		return 0;
+
+		final ExecutorService service = newFixedThreadPool(100);
+
+
+		try{
+			long total = 0;
+			final List<File> directories = new ArrayList<>();
+			directories.add(path);
+
+			while (!directories.isEmpty()) {
+
+			}
+
+
+			return 0;
+
+		}finally {
+			service.shutdown();
+		}
+
+
+
 	}
 
 
 
+
+
+
+
+
+
+	private SubDirectoriesAndSize getTotalAndSubDirs(final File file){
+		long total  = 0 ;
+		final List<File> subDirectories = new ArrayList<>();
+		if(file.isAbsolute()){
+			final File[] children = file.listFiles();
+			if(children != null){
+				for (File child : children) {
+					if(child.isFile()){
+						total += child.length();
+					}else{
+						subDirectories.add(child);
+					}
+				}
+			}
+		}
+
+		return new SubDirectoriesAndSize(total, subDirectories);
+	}
 
 
 
@@ -18,12 +68,13 @@ public class ConcurrentTotalFileSize {
 
 
 	class SubDirectoriesAndSize {
+		final public long size;
+		final public List<File> subDirectories;
+		public SubDirectoriesAndSize(long size, List<File> subDirectories) {
+			super();
+			this.size = size;
+			this.subDirectories = subDirectories;
+		}
 	}
-
-
-
-
-
-
 
 }
