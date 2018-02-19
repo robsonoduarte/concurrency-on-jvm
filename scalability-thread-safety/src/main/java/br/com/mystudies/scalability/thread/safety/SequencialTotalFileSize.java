360fad8a31acc3dffa4743a8eed376a7196eae61
Robson Duarte <robson.o.d@gmail.com>
2018-02-19 18:59:58 -0300

    format code.

--- scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/SequencialTotalFileSize.java
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/SequencialTotalFileSize.java
@@ -20,8 +20,6 @@ public class SequencialTotalFileSize {
 		return total;
 	}
 
-
-
 	public static void main(String[] args) throws IOException {
 		final long start = nanoTime();
 		final double total = new SequencialTotalFileSize().getTotalSizeOfFile(new File("F:"));
