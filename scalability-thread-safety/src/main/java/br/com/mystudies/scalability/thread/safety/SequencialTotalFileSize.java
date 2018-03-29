772b6fbb481009ae477d2c723f0e21c298039119
Robson Duarte <robson.o.d@gmail.com>
2018-03-28 21:23:12 -0300

    format code.

--- scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/SequencialTotalFileSize.java
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/SequencialTotalFileSize.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 public class SequencialTotalFileSize {
 
+	
 	public double getTotalSizeOfFile(File file) throws IOException {
 
 		if(file.isFile()) return file.length();
