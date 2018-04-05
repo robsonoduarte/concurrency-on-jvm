8defd650aed5fbe7de06276daeff6b25103a2553
Robson Duarte <robson.o.d@gmail.com>
2018-04-05 18:21:12 -0300

    format code.

--- scalability-thread-safety/src/test/java/br/com/mystudies/scalability/thread/safety/BaseTest.java
+++ scalability-thread-safety/src/test/java/br/com/mystudies/scalability/thread/safety/BaseTest.java
@@ -3,9 +3,7 @@ package br.com.mystudies.scalability.thread.safety;
 import java.io.File;
 
 public abstract class BaseTest {
-
 	protected File path() throws Exception {
 		return new File(BaseTest.class.getResource("/dir").toURI());
 	}		
-	
 }
