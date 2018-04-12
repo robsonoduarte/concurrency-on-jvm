43f0646267bf31497dba956f6133c15c7011fa61
Robson Duarte <robson.o.d@gmail.com>
2018-04-12 20:59:22 -0300

    format code.

--- scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/ConcurrentBlockingQueueTotalFileSize.java
+++ scalability-thread-safety/src/main/java/br/com/mystudies/scalability/thread/safety/ConcurrentBlockingQueueTotalFileSize.java
@@ -14,8 +14,7 @@ public class ConcurrentBlockingQueueTotalFileSize {
 	final private BlockingQueue<Long> fileSizes = new ArrayBlockingQueue<>(500);
 	final private AtomicLong pendingFileVisits = new AtomicLong();
 	final private ExecutorService service = newFixedThreadPool(100);
-	
-	
+		
 	public double getTotalSizeOfFile(final File file) throws InterruptedException {
 		try {
 			startExploreDir(file);
