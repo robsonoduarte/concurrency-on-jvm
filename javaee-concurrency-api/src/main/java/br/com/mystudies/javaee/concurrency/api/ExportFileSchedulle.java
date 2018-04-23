f40388ab04c679c90b745fc0caf96ce1e6d7676e
Robson Duarte <robson.o.d@gmail.com>
2018-04-23 15:47:38 -0300

    format code.

--- javaee-concurrency-api/src/main/java/br/com/mystudies/javaee/concurrency/api/ExportFileSchedulle.java
+++ javaee-concurrency-api/src/main/java/br/com/mystudies/javaee/concurrency/api/ExportFileSchedulle.java
@@ -2,10 +2,7 @@ package br.com.mystudies.javaee.concurrency.api;
 
 
 public class ExportFileSchedulle {
-
 	public ExportFilter exportFilter() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
